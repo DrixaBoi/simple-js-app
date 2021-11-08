e1878b03a2570b339570d9d2c353a6bb87f20ab8
Justin Eastin <worldmisanthropy199@gmail.com>
2021-11-08 15:52:23 -0500

    Update scripts.js
    Remove prior module's example code, to add beginning information for pokedex app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
-alert('Hello world');
-
-let favFood = 'Mexican';
-document.write(favFood);
+let pokemonList = [
+  {
+    name: 'Sandshrew',
+    height: 0.6,
+    weight: 12,
+    type: ['ground'],
+    weakness: ['water', 'grass', 'ice'],
+    evolutions: ['Sandslash']
+  },
+  {
+    name: 'Vulpix',
+    height: 0.6,
+    weight: 9.9,
+    type: ['fire'],
+    weakness: ['water', 'ground', 'rock'],
+    evolutions: ['Ninetails']
+  },
+  {
+    name: 'Oddish',
+    height: 0.5,
+    weight: 5.4,
+    type: ['grass', 'poison'],
+    weakness: ['ground', 'psychic', 'flying', 'fire', 'ice'],
+    evolutions: ['Gloom', 'Vileplume']
+  },
+  {
+    name: 'Mankey',
+    height: 0.5,
+    weight: 28,
+    type: ['fighting'],
+    weakness: ['flying', 'psychic', 'fairy'],
+    evolutions: ['Primeape']
+  },
+  {
+    name: 'Abra',
+    height: 0.9,
+    weight: 19.5,
+    type: ['psychic'],
+    weakness: ['bug', 'ghost', 'dragon'],
+    evolutions: ['Kadabra', 'Alakazam']
+  }
+];
