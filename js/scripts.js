b466f57ff5c59867df636e8010d9bf664ed5c579
Justin Eastin <worldmisanthropy199@gmail.com>
2021-11-10 19:23:09 -0500

    Pokemon info, and loop (if)
    add text to the index.html page detailing scanned pokemons information, and highlighting special pokemon according to height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
   {
     name: 'Sandshrew',
-    height: 0.6,
+    height: 0.8,
     weight: 12,
     type: ['ground'],
     weakness: ['water', 'grass', 'ice'],
@@ -17,7 +17,7 @@ let pokemonList = [
   },
   {
     name: 'Oddish',
-    height: 0.5,
+    height: 0.7,
     weight: 5.4,
     type: ['grass', 'poison'],
     weakness: ['ground', 'psychic', 'flying', 'fire', 'ice'],
@@ -25,7 +25,7 @@ let pokemonList = [
   },
   {
     name: 'Mankey',
-    height: 0.5,
+    height: 1.4,
     weight: 28,
     type: ['fighting'],
     weakness: ['flying', 'psychic', 'fairy'],
@@ -33,10 +33,28 @@ let pokemonList = [
   },
   {
     name: 'Abra',
-    height: 0.9,
+    height: 1.5,
     weight: 19.5,
     type: ['psychic'],
     weakness: ['bug', 'ghost', 'dragon'],
     evolutions: ['Kadabra', 'Alakazam']
   }
 ];
+
+document.write('<div class="pokeinfo">');
+  // Part 1, Create a for loop that iterates over each pokemon item in the list
+for (let i = 0; i < pokemonList.length; i++){
+  document.write('<div class="pokeinfo__item"><h2>' + pokemonList[i].name + '</h2>');
+  document.write('<h3>Height: ' + pokemonList[i].height + '</h3>');
+  document.write('<h3>Weight: ' + pokemonList[i].weight + '</h3>');
+  document.write('<h3>Type: ' + pokemonList[i].type + '</h3>');
+  document.write('<h3>Weakness: ' + pokemonList[i].weakness + '</h3>');
+  document.write('<h3>Evolutions: ' + pokemonList[i].evolutions + '</h3>');
+  // Part 3, add code to highlight special pokemon attributes.
+  if (pokemonList[i].height >= 1.5) {
+    document.write('<h3>  Pokedex: "Whoa! that\'s one big ' + pokemonList[i].name + '"</h3>');
+  // Part 3 extra, added a else for practice
+  }else {
+    document.write('<h3>  Pokedex: "This is an average size ' + pokemonList[i].name + '"</h3></div>')
+  }
+}
