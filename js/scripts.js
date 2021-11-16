ea8fd8fbe1a92a30a944a963a1560c8dc8826b2b
Justin Eastin <worldmisanthropy199@gmail.com>
2021-11-16 16:58:42 -0500

    Update scripts.js
    wrap pokemonList in an IIFE, Adjusted getAll to cope with changes, added new pokemon using add

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,81 @@
-let pokemonList = [
-  {
-    name: 'Sandshrew',
-    height: 0.8,
-    weight: 12,
-    type: ['ground'],
-    weakness: ['water', 'grass', 'ice'],
-    evolutions: ['Sandslash']
-  },
-  {
-    name: 'Vulpix',
-    height: 0.6,
-    weight: 9.9,
-    type: ['fire'],
-    weakness: ['water', 'ground', 'rock'],
-    evolutions: ['Ninetails']
-  },
-  {
-    name: 'Oddish',
-    height: 0.7,
-    weight: 5.4,
-    type: ['grass', 'poison'],
-    weakness: ['ground', 'psychic', 'flying', 'fire', 'ice'],
-    evolutions: ['Gloom', 'Vileplume']
-  },
-  {
-    name: 'Mankey',
-    height: 1.4,
-    weight: 28,
-    type: ['fighting'],
-    weakness: ['flying', 'psychic', 'fairy'],
-    evolutions: ['Primeape']
-  },
-  {
-    name: 'Abra',
-    height: 1.5,
-    weight: 19.5,
-    type: ['psychic'],
-    weakness: ['bug', 'ghost', 'dragon'],
-    evolutions: ['Kadabra', 'Alakazam']
+// IIFE wrap pokemonList
+let pokemonRepository = (function() {
+// Pokemon information list
+  let pokemonList = [
+    {
+      name: 'Sandshrew',
+      height: 0.8,
+      weight: 12,
+      type: ['ground'],
+      weakness: ['water', 'grass', 'ice'],
+      evolutions: ['Sandslash']
+    },
+    {
+      name: 'Vulpix',
+      height: 0.6,
+      weight: 9.9,
+      type: ['fire'],
+      weakness: ['water', 'ground', 'rock'],
+      evolutions: ['Ninetails']
+    },
+    {
+      name: 'Oddish',
+      height: 0.7,
+      weight: 5.4,
+      type: ['grass', 'poison'],
+      weakness: ['ground', 'psychic', 'flying', 'fire', 'ice'],
+      evolutions: ['Gloom', 'Vileplume']
+    },
+    {
+      name: 'Mankey',
+      height: 1.4,
+      weight: 28,
+      type: ['fighting'],
+      weakness: ['flying', 'psychic', 'fairy'],
+      evolutions: ['Primeape']
+    },
+    {
+      name: 'Abra',
+      height: 1.5,
+      weight: 19.5,
+      type: ['psychic'],
+      weakness: ['bug', 'ghost', 'dragon'],
+      evolutions: ['Kadabra', 'Alakazam']
+    }
+  ];
+// add function
+    function add(pokemon) {
+      if (typeof pokemon === 'object') {
+        pokemonList.push(pokemon);
+    }else{
+      alert('Not a valid entry')
+      }
+    }
+// getAll function
+    function getAll() {
+      return pokemonList;
+    }
+
+      return {
+        getAll: getAll,
+        add: add,
+      };
+  })();
+
+//Update loop code to cope with new changes
+function pokemonStats(pokemon) {
+  document.write('<div class="pokemon">' + pokemon.name + ' <br>height: ' + pokemon.height + ' <br>weight: ' + pokemon.weight + ' <br>type: ' + pokemon.type + ' <br>weakness: ' + pokemon.weakness + ' <br>evolutions: ' + pokemon.evolutions + '<br></div><br>');
+// Part 3, add code to highlight special pokemon attributes
+  if (pokemon.height >= 1.5) {
+    document.write('<div class="pokemon"> - Wow! That\'s big! </div><br>');
   }
-];
+}
 
-pokemonList.forEach(function(stats) {
-  document.write('<div class="pokemon">' + stats.name + ' is ' + stats.height + ' feet tall, weighing in at ' + stats.weight + ' pounds!<br>' + stats.type + ' is its type, with weaknesses to ' + stats.weakness + '.<br>Its evolutions are: ' + stats.evolutions + '<br></div><br>');
-});
+//Adding new pokemon entry
+pokemonRepository.add({name: 'Shelder', height: '0.6', weight: '2.3', type: ['water'], weakness: ['grass', 'electric'], evolutions: ['Cloyster']});
 
-
+//forEach function to show pokemonRepository contents
+pokemonRepository.getAll().forEach(pokemonStats);
 
 
 //document.write('<div class="pokeinfo">');
