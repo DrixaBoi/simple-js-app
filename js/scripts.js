ce4046ed31d48cc1fb2c6bb8b4b73dfe98510f57
Justin Eastin <worldmisanthropy199@gmail.com>
2021-11-16 15:12:20 -0500

    change from for to forEach
    change the for loop to a more compressed forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -41,20 +41,27 @@ let pokemonList = [
   }
 ];
 
-document.write('<div class="pokeinfo">');
+pokemonList.forEach(function(stats) {
+  document.write('<div class="pokemon">' + stats.name + ' is ' + stats.height + ' feet tall, weighing in at ' + stats.weight + ' pounds!<br>' + stats.type + ' is its type, with weaknesses to ' + stats.weakness + '.<br>Its evolutions are: ' + stats.evolutions + '<br></div><br>');
+});
+
+
+
+
+//document.write('<div class="pokeinfo">');
   // Part 1, Create a for loop that iterates over each pokemon item in the list
-for (let i = 0; i < pokemonList.length; i++){
-  document.write('<div class="pokeinfo__item"><h2>' + pokemonList[i].name + '</h2>');
-  document.write('<h3>Height: ' + pokemonList[i].height + '</h3>');
-  document.write('<h3>Weight: ' + pokemonList[i].weight + '</h3>');
-  document.write('<h3>Type: ' + pokemonList[i].type + '</h3>');
-  document.write('<h3>Weakness: ' + pokemonList[i].weakness + '</h3>');
-  document.write('<h3>Evolutions: ' + pokemonList[i].evolutions + '</h3>');
+//for (let i = 0; i < pokemonList.length; i++){
+//  document.write('<div class="pokeinfo__item"><h2>' + pokemonList[i].name + '</h2>');
+//  document.write('<h3>Height: ' + pokemonList[i].height + '</h3>');
+//  document.write('<h3>Weight: ' + pokemonList[i].weight + '</h3>');
+//  document.write('<h3>Type: ' + pokemonList[i].type + '</h3>');
+//  document.write('<h3>Weakness: ' + pokemonList[i].weakness + '</h3>');
+//  document.write('<h3>Evolutions: ' + pokemonList[i].evolutions + '</h3>');
   // Part 3, add code to highlight special pokemon attributes.
-  if (pokemonList[i].height >= 1.5) {
-    document.write('<h3>  Pokedex: "Whoa! that\'s one big ' + pokemonList[i].name + '"</h3></div>');
+//  if (pokemonList[i].height >= 1.5) {
+//    document.write('<h3>  Pokedex: "Whoa! that\'s one big ' + pokemonList[i].name + '"</h3></div>');
   // Part 3 extra, added a else for practice
-  }else {
-    document.write('<h3>  Pokedex: "This is an average size ' + pokemonList[i].name + '"</h3></div>')
-  }
-}
+//  }else {
+//    document.write('<h3>  Pokedex: "This is an average size ' + pokemonList[i].name + '"</h3></div>')
+//  }
+//}
